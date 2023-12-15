a75beaa63e02207f9f2bbba69b43a3a356773239
David Álvarez Rosa <david@alvarezrosa.com>
2023-12-15 15:05:34 +0100

    Add basic integration with Emacs projectile mode for SwiftPlayground

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . (
+         (projectile-project-compilation-cmd . "swift build")
+         (projectile-project-test-cmd . "swift test")
+         (projectile-project-run-cmd . "swift run"))))
