c774cf8f955d5da80c6e9baf230988160032ccd6
David Álvarez Rosa <david@alvarezrosa.com>
2023-12-15 15:19:52 +0100

    Fix build and run erros

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,12 @@
+class User {
+    var name : String
+    var age : Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
 print("Hello, world!")
+// let user = User()
